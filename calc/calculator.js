82ca60892ea1302a71b7bc740f9c239ec24d675e
Hoor Hanif <105176516+HoorHanif@users.noreply.github.com>
2024-03-20 12:21:19 +0500

    BMI Calculator

--- calc/calculator.js
+++ calc/calculator.js
@@ -2,4 +2,3 @@ var weight = 54;
 var height = 1.5493;
 var bmi = weight / (height * height);
 console.log("your bmi is ".concat(bmi));
-764;
--- calc/calculator.ts
+++ calc/calculator.ts
@@ -1,4 +1,4 @@
 let weight=54;
 let height =1.5493;
 let bmi= weight/(height*height);
-console.log(`your bmi is ${bmi}`);764
+console.log(`your bmi is ${bmi}`);

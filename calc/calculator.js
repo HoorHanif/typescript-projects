f4ecc2347dded19b9ac41a3ded73d774534e0bc6
Hoor Hanif <105176516+HoorHanif@users.noreply.github.com>
2024-03-20 12:23:45 +0500

    Delete calc directory

--- calc/calculator.js
+++
@@ -1,4 +0,0 @@
-var weight = 54;
-var height = 1.5493;
-var bmi = weight / (height * height);
-console.log("your bmi is ".concat(bmi));
--- calc/calculator.ts
+++
@@ -1,4 +0,0 @@
-let weight=54;
-let height =1.5493;
-let bmi= weight/(height*height);
-console.log(`your bmi is ${bmi}`);
